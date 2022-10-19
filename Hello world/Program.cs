e05118a7766f65511ae8d8c4af6d1d35e6a1d827
User <User@WIN-99IIJ6VKAED>
2022-10-19 16:17:18 +0500

    chanche

--- Hello world/Program.cs
+++ Hello world/Program.cs
@@ -3,5 +3,5 @@ string name = Console.ReadLine();
 Console.WriteLine(name);
 Console.WriteLine("Сколько вам лет?");
 string name2 = Console.ReadLine();
-Console.WriteLine("Добрый день, " + name + " вам " + name2);
+Console.WriteLine("Добрый день,Добро пожаловать " + name + " вам " + name2);
 Console.ReadLine();
