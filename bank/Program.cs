1bcdff3b7802b47771dbdc9622589cad3ed96eaa
User <User@WIN-99IIJ6VKAED>
2022-11-06 17:11:46 +0500

    bank

---
+++ bank/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите сумму:");
+double sum = double.Parse(Console.ReadLine());
+Console.WriteLine("Виберите количества месяца:");
+int monthcount = int.Parse(Console.ReadLine());
+double sumbefore = sum;
+for (int i = 1; i <= monthcount; i++)
+{
+    double porsent = sum * 0.07;
+    sum +=porsent;
+    Console.WriteLine($"{i} месяц-{sum}");
+
+}
+double pribil = sum - sumbefore;
+Console.WriteLine($"Ваш прибыл {pribil}");
